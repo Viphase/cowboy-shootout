967ae603fc87c16527312857c37d03ea224dd45b
Viphase <9374143431@mail.ru>
2025-12-01 20:03:26 +0300

    adding-base-tracking-and-facade

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import cv2
+from mp.facade import MediaPipeFacade
+
+cap = cv2.VideoCapture(0)
+mp_facade = MediaPipeFacade()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = mp_facade.process_frame(frame)
+    cv2.imshow("testik", frame)
+
+mp_facade.close()
+cap.release()
+cv2.destroyAllWindows()
